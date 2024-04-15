45aa1717a78b97cf90f95b524fa98fd5d992d1d7
Christian Engler <caeycae@gmail.com>
2024-04-15 00:57:10 -0300

    optionals

---
+++ src/main/scala/lectures/part3functional/C34Options.scala
@@ -0,0 +1,87 @@
+package com.despegar
+package lectures.part3functional
+
+import scala.util.Random
+
+object C34Options extends App {
+
+  val myFirstOption: Option[Int] = Some(4)
+  val noOption: Option[Int] = None
+
+  println(myFirstOption)
+
+  // WORK with unsafe APIs
+  def unsafeMethod(): String = null
+
+  //  val result = Some(null) // WRONG
+  val result = Option(unsafeMethod()) // Some or None
+  println(result)
+
+  // chained methods
+  def backupMethod(): String = "A valid result"
+
+  val chainedResult = Option(unsafeMethod()).orElse(Option(backupMethod()))
+
+  // DESIGN unsafe APIs
+  def betterUnsafeMethod(): Option[String] = None
+
+  def betterBackupMethod(): Option[String] = Some("A valid result")
+
+  val betterChainedResult = betterUnsafeMethod() orElse betterBackupMethod()
+
+  // functions on Options
+  println(myFirstOption.isEmpty)
+  println(myFirstOption.get) // USAFE - DO NOT USE THIS
+
+  // map, flatMap, filter
+  println(myFirstOption.map(_ * 2))
+  println(myFirstOption.filter(x => x > 10))
+  println(myFirstOption.flatMap(x => Option(x * 10)))
+
+  // for-comprehensions
+
+  /*
+    Exercise.
+   */
+  val config: Map[String, String] = Map(
+    // fetched from elsewhere
+    "host" -> "176.45.36.1",
+    "port" -> "80"
+  )
+
+  class Connection {
+    def connect = "Connected" // connect to some server
+  }
+
+  object Connection {
+    val random = new Random(System.nanoTime())
+
+    def apply(host: String, port: String): Option[Connection] =
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+  }
+
+  // try to establish a connection, if so - print the connect method
+
+  val host = config.get("host")
+  val port = config.get("port")
+  val connection = host.flatMap(h => port.flatMap(p => Connection.apply(h, p)))
+  val connectionStatus = connection.map(c => c.connect)
+  connectionStatus.foreach(println)
+
+  // chained calls
+  config.get("host")
+    .flatMap(host => config.get("port")
+      .flatMap(port => Connection(host, port))
+      .map(connection => connection.connect))
+    .foreach(println)
+
+  // for-comprehensions
+  val forConnectionStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+
+  forConnectionStatus.foreach(println)
+}
